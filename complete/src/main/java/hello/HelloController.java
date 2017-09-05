7c84e4201018bea785b5d1e2187c2267df67b458
Franck Valot <valot@et.esiea.fr>
2017-09-05 21:17:34 +0700

    index.html

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,7 +1,5 @@
 package hello;
 
-//import java.util.ArrayList;
-//import java.util.List;
 import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     
-    //ArrayList list = new ArrayList();
     
     @RequestMapping("/")
     public String index() {
-        //list.add("toto");
         return "index";
     }
 }
