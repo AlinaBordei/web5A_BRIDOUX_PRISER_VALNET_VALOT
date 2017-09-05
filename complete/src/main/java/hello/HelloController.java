cde04a16aed37336ec0fa1e8120410e3a2ec008e
Alina <bordei@et.esiea.fr>
2017-09-05 15:56:11 +0200

    Test affichage index.html

--- complete/src/main/java/hello/HelloController.java
+++ complete/src/main/java/hello/HelloController.java
@@ -1,14 +1,19 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+//import java.util.ArrayList;
+//import java.util.List;
+import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
+    
+    //ArrayList list = new ArrayList();
     
     @RequestMapping("/")
     public String index() {
-        return "alina a des problèmes!";
+        //list.add("toto");
+        return "index";
     }
-    
 }
