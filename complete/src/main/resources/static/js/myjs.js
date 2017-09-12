d19e03345033366f936ab98f0e15c0a0b2a4a070
Franck Valot <valot@et.esiea.fr>
2017-09-12 20:18:07 +0700

    Update navbar

--- complete/src/main/resources/static/js/myjs.js
+++ complete/src/main/resources/static/js/myjs.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$( ".signin-link" ).click(function() {
 		$( "#signup-form" ).hide();
-		$( "#signin-form" ).show(200);
+		$( "#signin-form" ).fadeIn(200);
 	});
 
 	$( ".signup-link" ).click(function() {
 		$( "#signin-form" ).hide();
-		$( "#signup-form" ).show(200);
+		$( "#signup-form" ).fadeIn(200);
 	});
 });
