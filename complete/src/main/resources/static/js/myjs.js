6d7f43fa82342a8dd1405494e53be2f217abfc06
Franck Valot <valot@et.esiea.fr>
2017-09-12 17:52:03 +0700

    Update index view

--- complete/src/main/resources/static/js/myjs.js
+++ complete/src/main/resources/static/js/myjs.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	$( ".signin-link" ).click(function() {
 		$( "#signup-form" ).hide();
-		$( "#signin-form" ).show(300);
+		$( "#signin-form" ).show(200);
 	});
 
 	$( ".signup-link" ).click(function() {
 		$( "#signin-form" ).hide();
-		$( "#signup-form" ).show(300);
+		$( "#signup-form" ).show(200);
 	});
 });
