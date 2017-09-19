9988551b4b0867acf37cf98902f86775912656de
Clara <bridoux@et.esiea.fr>
2017-09-19 14:05:51 +0200

    Insert data in db with ajax --> new user

--- complete/src/main/resources/static/js/myjs.js
+++ complete/src/main/resources/static/js/myjs.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
 	
 	$( "#signup-btn" ).click(function() {
-		$( "#signup-form" ).hide();
-		$( "#signin-form" ).fadeIn(200);
+		var uname = $("#userName").val();
+		var pwd = $("#password").val();
+		var email = $("#mail").val();
+
+		var dataString = {userName:uname, password:pwd, mail:email};
+		addNewUser(dataString);
+		/*$( "#signup-form" ).hide();
+		$( "#signin-form" ).fadeIn(200);*/
 	});
 
 	$( ".signup-link" ).click(function() {
@@ -10,6 +16,20 @@ $(document).ready(function() {
 		$( "#signup-form" ).fadeIn(200);
 	});
 	
+	function addNewUser(dataString){
+		$.ajax({
+		  headers:{
+			  'Accept': 'application/json',
+			  'Content-Type': 'application/json'
+		  },
+		  type: "POST",
+		  url: "http://localhost:8080/addUser",
+		  data: JSON.stringify(dataString),
+		  dataType: 'json'
+		}).done(function(){
+			alert("Tu es enregistré !");	
+		});	
+	}	
 
 	$( ".signin-link" ).click(function() {
 		$( "#signup-form" ).hide();
