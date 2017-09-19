957b1d92c0be92eb48e9fd19a33198ab9b13e86a
priser <priser@et.esiea.fr>
2017-09-19 16:08:22 +0200

    Modif controller

--- complete/src/main/java/hello/ConversationController.java
+++ complete/src/main/java/hello/ConversationController.java
@@ -12,64 +12,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import repository.UserRepository;
-import model.User;
+import repository.ConversationRepository;
+import model.Conversation;
 
 // TODO
 //
 //
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-/*
+
+
 //Rest for transferring data
 @RestController
 public class ConversationController {	
 	@Inject
-	UserRepository userRepo;
+	ConversationRepository convRepo;
 	
-	//Get all users
-    @GetMapping("/users")
-    public @ResponseBody List<User> allUsers() {
-    	return userRepo.allUsers();
+	//Get all conversation
+    @GetMapping("/conversation")
+    public @ResponseBody List<Conversation> allConversation() {
+    	return convRepo.allConversation();
     }
-    
-    //Get user by id
-    @GetMapping("/usersById/{id}")
-    public @ResponseBody User userById(@PathVariable("id") Integer id) {
-    	userRepo.validateUser(id);
-    	return userRepo.userById(id);
+
+    //Get conversation by id
+    @GetMapping("/ConversationById/{id}")
+    public @ResponseBody List<Conversation> ConversationById(int id) {
+    	return convRepo.ConversationById(id);
     }
-    
-    
-    
-    
-    
-    //Add user
-    @GetMapping("/addUser/{name}/{password}/{mail}")
-    public @ResponseBody int addUser(@PathVariable("name") String name,@PathVariable("password") String password, @PathVariable("mail") String mail) {
-    	//Return 1 when it's ok
-    	return userRepo.addUser(name, password, mail);
-    }
-    
-    //To test
-    @PostMapping("/addUser2")
-    public @ResponseBody int addUserJson(@RequestBody User user) {
-    	String name = user.getUserName();
-    	String password = user.getPassword();
-    	String mail = user.getMail();
-      //Return 1 when it's ok
-    	return userRepo.addUser(name, password, mail);
-    }
-}*/
+}
